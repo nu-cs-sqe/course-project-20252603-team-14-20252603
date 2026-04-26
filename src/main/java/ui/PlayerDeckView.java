bc0a6036c505a18d64e748f293ab30a9096ce30c
jazlynho2028 <jazlynho2028@u.northwestern.edu>
2026-04-26 16:22:24 -0500

    Refactored gameBoardSection and playerChoiceSection into separate methods

--- src/main/java/ui/PlayerDeckView.java
+++ src/main/java/ui/PlayerDeckView.java
@@ -7,6 +7,8 @@ import javafx.scene.layout.VBox;
 public class PlayerDeckView {
 
     private VBox root;
+    private VBox gameBoardSection;
+    private VBox playerChoiceSection;
 
     public PlayerDeckView(PlayerDeckController controller) {
         buildUI(controller);
@@ -19,15 +21,22 @@ public class PlayerDeckView {
     private void buildUI(PlayerDeckController controller) {
         root = new VBox();
 
-        VBox gameBoardSection = new VBox();
-        gameBoardSection.getStyleClass().add("game-board-section");
-        gameBoardSection.getChildren().add(new Label("gameBoardSection"));
-
-        VBox playerChoiceSection = new VBox();
-        playerChoiceSection.getStyleClass().add("player-choice-section");
-        playerChoiceSection.getChildren().add(new Label("playerChoiceSection"));
+        buildGameBoardSection(controller);
+        buildPlayerChoiceSection(controller);
 
         root.getChildren().addAll(gameBoardSection, playerChoiceSection);
+    }
+
+    private void buildGameBoardSection(PlayerDeckController controller) {
+        gameBoardSection = new VBox();
+        gameBoardSection.getStyleClass().add("game-board-section");
+        gameBoardSection.getChildren().add(new Label("gameBoardSection"));
+    }
+
+    private void buildPlayerChoiceSection(PlayerDeckController controller) {
+        playerChoiceSection = new VBox();
+        playerChoiceSection.getStyleClass().add("player-choice-section");
+        playerChoiceSection.getChildren().add(new Label("playerChoiceSection"));
     }
 
 }
