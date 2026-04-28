d3b14bd0598de541b4b098fc125d7afc12aecb48
Luke Meyer <lmeyer0215@gmail.com>
2026-04-28 03:56:11 -0500

    controller noew gets the view when called and we can start on allowing it to switch to the player setup view

--- src/main/java/ui/StartController.java
+++ src/main/java/ui/StartController.java
@@ -1,7 +1,19 @@
 package ui;
 
 public class StartController {
-    public void startGame() {
+
+    private final SceneManager sceneManager;
+    private final StartView view;
+
+    public StartController(AssetManager assets, SceneManager sceneManager) {
+        this.sceneManager = sceneManager;
+        this.view = new StartView(this, assets);
     }
 
+    public StartView getStartView() {
+        return view;
+    }
+    public void startGame() {
+        //sceneManager.showPlayerSetupScreen();
+    }
 }
