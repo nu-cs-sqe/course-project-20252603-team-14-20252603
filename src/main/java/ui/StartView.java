a4628de6585c90d148cbe39a9f3d55b0bc482f5f
Luke Meyer <lmeyer0215@gmail.com>
2026-04-28 02:10:00 -0500

    initial instantiation of variables as well as a blank controller class for no errors

--- src/main/java/ui/StartView.java
+++ src/main/java/ui/StartView.java
@@ -1,6 +1,29 @@
 package ui;
 
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 public class StartView {
+    private final StartController controller;
+    private final AssetManager assets;
+    private StackPane root;
+
+    public StartView(StartController controller, AssetManager assets){
+        this.controller = controller;
+        this.assets = assets;
+        buildUI();
+    }
+
+    public Parent getRoot() { return root; }
+
+    private void buildUI() {
+
+    }
 
 }
