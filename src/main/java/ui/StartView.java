adaf9faf398ca095616c095702bf557df0362f9f
Luke Meyer <lmeyer0215@gmail.com>
2026-05-03 22:58:41 -0500

    changed startview to no longer use UIGradients

--- src/main/java/ui/StartView.java
+++ src/main/java/ui/StartView.java
@@ -10,8 +10,10 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
 public class StartView {
+
     private final StartController controller;
     private final AssetManager assets;
+
     private StackPane root;
 
     public StartView(StartController controller, AssetManager assets){
@@ -24,84 +26,71 @@ public class StartView {
 
     private void buildUI() {
         root = new StackPane();
+        root.getStyleClass().add("root");
+
         StackPane startScreen = buildStartScreen();
         root.getChildren().add(startScreen);
     }
 
     private StackPane buildStartScreen() {
         StackPane startScreen = new StackPane();
-        startScreen.getStyleClass().add("start-screen");
 
         VBox content = new VBox();
         content.setAlignment(Pos.CENTER);
-        content.setSpacing(30);
+        content.setSpacing(48);
+        content.getStyleClass().add("content-section");
 
         VBox titleBox = buildExplodingKittensText();
         ImageView explosionCat = buildExplosionImage();
         Button playButton = buildPlayButton();
 
-        content.getChildren().addAll(titleBox,explosionCat,playButton);
+        content.getChildren().addAll(titleBox, explosionCat, playButton);
         startScreen.getChildren().add(content);
+
         return startScreen;
-
     }
-
 
     private ImageView buildExplosionImage() {
         Image image = assets.getImage("placeholder");
         ImageView imageView = new ImageView(image);
 
-        imageView.setFitWidth(500);
+        imageView.setFitWidth(400);
         imageView.setPreserveRatio(true);
 
         return imageView;
     }
 
     private Button buildPlayButton() {
-        Button startButton = new Button("");
+        Button button = new Button("Play");
+        button.getStyleClass().addAll("name-tag", "h4");
 
-        Text startButtonText = buildStartText();
-        startButton.setGraphic(startButtonText);
-        startButton.getStyleClass().add("name-tag");
+        button.setOnAction(e -> controller.startGame());
 
-        return startButton;
-    }
-
-
-    private Text buildStartText() {
-        Text startText = new Text("Play");
-        startText.setFill(UIGradients.GRADIENT_2);
-        startText.getStyleClass().add("h4");
-
-        return startText;
+        return button;
     }
 
     private VBox buildExplodingKittensText() {
-        VBox explodingKittensText = new VBox();
-        explodingKittensText.setAlignment(Pos.CENTER);
+        VBox box = new VBox();
+        box.setAlignment(Pos.CENTER);
+        box.getStyleClass().add("exploding-kittens-text");
 
-        String title_words = UIConstants.TITLE;
-        Text explodingText = buildExplodingText(title_words);
+        Text exploding = buildExplodingText(UIConstants.TITLE);
 
-        explodingKittensText.getChildren().addAll(explodingText);
-
-        return explodingKittensText;
+        box.getChildren().addAll(exploding);
+        return box;
     }
 
     private Text buildExplodingText(String text) {
-        Text explodingText = new Text(text);
-        explodingText.setFill(UIGradients.GRADIENT_1);
-        explodingText.getStyleClass().add("h7");
+        Text t = new Text(text);
+        t.getStyleClass().addAll("exploding-text", "h3");
 
-        return explodingText;
+        return t;
     }
 
     private Text buildKittensText(String text) {
-        Text kittensText = new Text(text);
-        kittensText.getStyleClass().addAll("kittens-text", "h3");
+        Text t = new Text(text);
+        t.getStyleClass().addAll("kittens-text", "h3");
 
-        return kittensText;
+        return t;
     }
-
-
 }
