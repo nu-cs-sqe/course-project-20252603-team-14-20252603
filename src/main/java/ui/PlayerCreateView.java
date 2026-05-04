6673a954291e66d6813656560644cf788bf6ccd1
Luke Meyer <lmeyer0215@gmail.com>
2026-05-04 00:07:18 -0500

    root method as well as the title section created

--- src/main/java/ui/PlayerCreateView.java
+++ src/main/java/ui/PlayerCreateView.java
@@ -14,4 +14,36 @@ import javafx.scene.text.Text;
 
 public class PlayerCreateView {
 
+    private final AssetManager assets;
+
+    private StackPane root;
+    private VBox playerFieldsContainer;
+    private Button addPlayerButton;
+    private Button confirmButton;
+    private Button backButton;
+
+
+    private static final int MAX_PLAYERS = 5;
+
+    public PlayerCreateView(AssetManager assets) {
+        this.assets = assets;
+    }
+
+    public Parent getRoot() {
+        return root;
+    }
+
+
+    private VBox buildTitleSection() {
+        VBox box = new VBox();
+        box.setAlignment(Pos.CENTER);
+
+        Text title = new Text(UIConstants.TITLE);
+        title.getStyleClass().addAll("exploding-text", "h7");
+
+        box.getChildren().add(title);
+        return box;
+    }
+
+
 }
