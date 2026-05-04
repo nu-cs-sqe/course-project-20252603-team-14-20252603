a80ba316b76be33b83f409f75b618200a5c7a9de
Luke Meyer <lmeyer0215@gmail.com>
2026-05-04 02:03:46 -0500

    initialize necessary controller methods

--- src/main/java/ui/PlayerCreateController.java
+++ src/main/java/ui/PlayerCreateController.java
@@ -1,8 +1,29 @@
 package ui;
 
 
-
+import java.util.List;
 
 public class PlayerCreateController {
 
+    private final SceneManager sceneManager;
+    private final PlayerCreateView view;
+
+    public PlayerCreateController(AssetManager assets, SceneManager sceneManager) {
+        this.sceneManager = sceneManager;
+        this.view = new PlayerCreateView(assets);
+
+        bindUI();
+    }
+
+    public PlayerCreateView getView() {
+        return view;
+    }
+
+    private void bindUI() {
+
+    }
+
+    private void startGame(List<String> names) {
+
+    }
 }
