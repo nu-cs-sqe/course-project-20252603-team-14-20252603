3a1ece07c2768ee52d1eceb208aa996095f631ac
jazlynho2028 <jazlynho2028@u.northwestern.edu>
2026-05-10 12:19:08 -0500

    Removed files outside of main and card metadata

--- src/main/java/ui/ExplodingKittensApp.java
+++ src/main/java/ui/ExplodingKittensApp.java
@@ -1,6 +1,5 @@
 package ui;
 
-import domain.Game;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -11,33 +10,12 @@ public class ExplodingKittensApp extends Application {
 
     @Override
     public void start(Stage stage) {
-        Game model = new Game();
         assets.loadGlobalFiles();
 
-        showPlayerDeckScreen(model, stage);
-    }
-
-    private void showStartScreen(Stage stage) {
         StartController controller = new StartController(assets);
 
         Scene startScene = controller.getStartScene();
         setScene(startScene, stage);
-    }
-
-    private void showPlayerDeckScreen(Game model, Stage stage) {
-        PlayerDeckController controller = new PlayerDeckController(model, assets);
-
-        controller.setOnError(message -> showErrorScreen(message, stage));
-
-        Scene playerDeckScene = controller.getPlayerDeckScene();
-        setScene(playerDeckScene, stage);
-    }
-
-    private void showErrorScreen(String message, Stage stage) {
-        ErrorController controller = new ErrorController(message);
-
-        Scene errorScene = controller.getErrorScene();
-        setScene(errorScene, stage);
     }
 
     private void setScene(Scene scene, Stage stage) {
