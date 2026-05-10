e2d297c9d58c88b1f5b595ad7cdd65ca85b75784
jazlynho2028 <jazlynho2028@u.northwestern.edu>
2026-05-10 12:25:39 -0500

    Removed unused constants in UIConstants

--- src/main/java/ui/UIConstants.java
+++ src/main/java/ui/UIConstants.java
@@ -5,17 +5,7 @@ public final class UIConstants {
     private UIConstants() { }
 
     public static final String TITLE = "Exploding Kittens";
-    public static final String ERROR_MESSAGE_TITLE = "AN ERROR OCCURRED";
     public static final String PLAY_BUTTON_LABEL = "PLAY";
-    public static final String PLAYER_HEADER_CAPTION = "CLICK TO SEE EACH PLAYER'S CARDS";
-    public static final String DRAW_PILE_CAPTION = "CLICK TO DRAW";
-    public static final String DISCARD_PILE_CAPTION = "DISCARD";
-    public static final String HIDE_HAND_LABEL = "HIDE";
-    public static final String SHOW_HAND_LABEL = "REVEAL";
-    public static final String HAND_CAPTION = "SCROLL TO SEE ALL CARDS";
-    public static final String PLAY_CARDS_LABEL = "PLAY CARD(S)";
-    public static final String START_GAME_LABEL = "START GAME";
-    public static final String END_TURN_LABEL = "END TURN";
 
     public static final int LOADED_FONT_SIZE = 12;
 
@@ -23,14 +13,5 @@ public final class UIConstants {
     public static final int SCENE_HEIGHT = 832;
 
     public static final int BACKGROUND_IMAGE_WIDTH = 500;
-
-    public static final int SCREEN_OVERLAY_INSETS = 8;
-
-    public static final int CARD_BACK_ICON_WIDTH = 40;
-    public static final int CARD_DESCRIPTION_WRAPPING_WIDTH = 100;
-
-    public static final int CARD_IMAGE_WIDTH = 140;
-    public static final int CARD_IMAGE_HEIGHT = 154;
-    public static final double CARD_IMAGE_BOTTOM_PADDING = 10;
 
 }
